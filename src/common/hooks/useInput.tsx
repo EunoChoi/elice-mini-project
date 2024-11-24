e8df19955017dd49368bc1f3f6a56a83276323f3
euno <chonist@gmail.com>
2024-11-24 22:19:47 +0900

    feat: useInput hooks 추가

---
+++ src/common/hooks/useInput.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const useInput = (initialValue: string) => {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+  const reset = () => {
+    setValue('');
+  };
+
+  return { value, onChange, reset };
+}
+
+export default useInput;
