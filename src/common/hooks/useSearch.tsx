abd136440537278cd2e6e02db0953eb608928fa1
euno <chonist@gmail.com>
2024-11-25 10:32:45 +0900

    refactor: 불필요한 코드 정리

--- src/common/hooks/useSearch.tsx
+++ src/common/hooks/useSearch.tsx
@@ -1,26 +1,8 @@
-// import { CHIPS, CHIPS_ARRAY } from '@/constants/CHIPS';
-// import { QueryKey } from '@/types/QueryKey';
-// import {
-//   addEachQueryValue,
-//   alreadyExistQueryValue,
-//   removeEachQueryValue,
-// } from '@/utils/routerQueryString';
-
-
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { getChipsCondition } from '../functions/getChipsCondition';
 import { makeUrl } from '../functions/makeUrl';
 import { getCourses } from '@/app/(elice-edu-course)/_api/getCourse';
-
-//여기에 stringify함수 넣고
-//함수 거친 값을 router.push하면된다. 
-
-// name: string;
-// query_key: string;
-// query_value: string;
-// enroll_type: number;
-// is_free: boolean;
 
 interface chipData {
   name: string;
@@ -49,7 +31,6 @@ export default function useSearch() {
   const offset = 20 * (current - 1);
 
 
-
   const updateCourses = async () => {
     const response = await getCourses(offset, count, search, chips);
     setResult(response);
@@ -62,18 +43,6 @@ export default function useSearch() {
     router.push(url);
     updateCourses();
   }, [search, chips, offset, current]);
-
-
-
-  console.log(chips);
-
-  // const add = (chipData: chipData) => {
-
-  // };
-
-  // const remove = (key: QueryKey, value: string) => {
-
-  // };
 
   return { search, setSearch, chips, setChips, result, current, setCurrent, total };
 }
