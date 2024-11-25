bf1665221cf6d2e68c7ef37db8d5247a7b4a8952
euno <chonist@gmail.com>
2024-11-25 10:25:34 +0900

    feat:useInput hooks 내부 debounce 기능 추가

--- src/common/hooks/useInput.tsx
+++ src/common/hooks/useInput.tsx
@@ -1,16 +1,38 @@
-import { useState } from "react";
+'use client';
 
-const useInput = (initialValue: string) => {
-  const [value, setValue] = useState(initialValue);
+import { useState, useEffect } from "react";
+
+interface UseInputReturn {
+  value: string;
+  debouncedValue: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  reset: () => void;
+}
+
+const useInput = (initialValue: string, delay: number = 500): UseInputReturn => {
+  const [value, setValue] = useState<string>(initialValue);
+  const [debouncedValue, setDebouncedValue] = useState<string>(initialValue);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
+
   const reset = () => {
     setValue('');
+    setDebouncedValue('');
   };
 
-  return { value, onChange, reset };
-}
+  return { value, debouncedValue, onChange, reset };
+};
 
 export default useInput;
