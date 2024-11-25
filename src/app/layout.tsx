265e46627df8cd168fb6ce8cf42ca5e7484c9888
euno <chonist@gmail.com>
2024-11-25 11:57:15 +0900

    feat: reactQuery Provider 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import StyledComponentsRegistry from './lib/registry'
+import RQProvider from "@/reactQuery/RQProvider";
 
 
 export const metadata: Metadata = {
@@ -17,7 +18,11 @@ export default function RootLayout({
       <body>
         <link href="https://font.elice.io/css?family=Elice+DX+Neolli" rel="stylesheet" />
         <link rel="stylesheet" as="style" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css" />
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <StyledComponentsRegistry>
+          <RQProvider>
+            {children}
+          </RQProvider>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
